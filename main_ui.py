d60fdd17845ef0e1fa64445bfb99d96e3e786351
carsupper665 <c112156118@nkust.edu.tw>
2025-09-09 02:45:18 +0800

    initial commit

---
+++ main_ui.py
@@ -0,0 +1,145 @@
+import sys
+from PyQt6.QtCore import Qt, QEvent
+from PyQt6.QtGui import QPalette, QIcon
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QStyle,
+    QToolButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+from src import constants as c
+
+# 建立主視窗類別
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.resize(1280, 720)
+        self.rework_window_hit()
+
+    def rework_window_hit(self):
+        self.main_container = QWidget()
+        self.main_container.setStyleSheet("background-color: #00081C;")
+        self.main_layout = QVBoxLayout(self.main_container)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+
+        # 使用自訂 TitleBar
+        self.titlebar = CustomTitleBar(self)
+
+        self.main_layout.addWidget(self.titlebar)
+        self.main_layout.addStretch()
+
+        self.setCentralWidget(self.main_container)           # 設定視窗大小
+
+class CustomTitleBar(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.is_parent_max = False
+        self.parent = parent
+        self.setAutoFillBackground(True)
+        self.setBackgroundRole(QPalette.ColorRole.Highlight)
+        self.setFixedHeight(45)
+        self.initial_pos = None
+        self._mouse_pos = None
+        title_bar_layout = QHBoxLayout(self)
+        title_bar_layout.setContentsMargins(1, 1, 1, 1)
+        title_bar_layout.setSpacing(2)
+        self.title = QLabel(f"M。P。M。C", self)
+        self.title.setStyleSheet(
+            """font-weight: bold;
+               border: 2px solid white;
+               border-radius: 12px;
+               margin: 2px;
+               background-color: black;
+            """
+        )
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        if title := parent.windowTitle():
+            self.title.setText(title)
+        title_bar_layout.addWidget(self.title)
+        self.min_button = QToolButton(self)
+        min_icon = QIcon()
+        min_icon.addFile(c.IMAGES_URL + '/min_btn.png')
+        self.min_button.setIcon(min_icon)
+        self.min_button.clicked.connect(self.parent.showMinimized)
+
+        # Max button
+        self.max_button = QToolButton(self)
+        max_icon = QIcon()
+        max_icon.addFile(c.IMAGES_URL + "/max_btn.png")
+        self.max_button.setIcon(max_icon)
+        self.max_button.clicked.connect(self.change_window)
+
+        # Close button
+        self.close_button = QToolButton(self)
+        close_icon = QIcon()
+        close_icon.addFile(c.IMAGES_URL + "/close_btn.png")
+        self.close_button.setIcon(close_icon)
+        self.close_button.clicked.connect(self.parent.close)
+
+        # Normal button
+        self.normal_button = QToolButton(self)
+        self.normal_button.setIcon(max_icon)
+        self.normal_button.clicked.connect(self.change_window)
+        self.normal_button.setVisible(False)
+        buttons = [
+            self.min_button,
+            self.normal_button,
+            self.max_button,
+            self.close_button,
+        ]
+        for button in buttons:
+            button.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+            button.setFixedSize(QSize(42, 42))
+            button.setIconSize(QSize(24, 24))
+            button.setStyleSheet("""QToolButton { 
+                                 border: none;
+                                 border-radius: 20px;}""" + "\n" + c.QTOOLBUTTON_HOVER
+            )
+            title_bar_layout.addWidget(button)
+        self.close_button.setStyleSheet(
+                """QToolButton { border: none;
+                                 border-radius: 20px;
+                                }
+                    QToolButton:hover {
+                    background-color:  rgba(229, 34, 34, 255); 
+                }""" )
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self._mouse_pos = event.globalPosition().toPoint()
+
+    def mouseMoveEvent(self, event):
+        if self._mouse_pos is not None:
+            delta = event.globalPosition().toPoint() - self._mouse_pos
+            self.parent.move(self.parent.x() + delta.x(),
+                             self.parent.y() + delta.y())
+            self._mouse_pos = event.globalPosition().toPoint()
+
+    def mouseReleaseEvent(self, event):
+        self._mouse_pos = None
+
+    def change_window(self,):
+        if self.is_parent_max:
+            self.is_parent_max = False
+            self.parent.showNormal()
+            self.max_button.setVisible(True)
+            self.normal_button.setVisible(False)
+        else:
+            self.parent.showMaximized()
+            self.is_parent_max = True
+            self.max_button.setVisible(False)
+            self.normal_button.setVisible(True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)   # 建立應用程式物件
+    window = MainWindow()          # 建立主視窗
+    window.show()                  # 顯示視窗
+    sys.exit(app.exec())           # 進入事件迴圈
---
+++ src/constants.py
@@ -0,0 +1,8 @@
+BASE_API = r"https://mc.yyanc9.com/api/"
+IMAGES_URL = r"./src/images"
+
+QTOOLBUTTON_HOVER = """
+QToolButton:hover {
+        background-color:  rgba(255, 255, 255, 50); 
+    }
+"""
